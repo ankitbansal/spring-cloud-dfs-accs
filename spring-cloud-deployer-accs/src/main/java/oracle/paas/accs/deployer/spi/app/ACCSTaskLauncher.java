34b5bd5b9f17c2ee655d5276f1dedb8a07d4662a
Ankit Bansal <ankit.a.bansal@oracle.com>
2018-02-05 03:23:11 -0800

    Task related changes

--- spring-cloud-deployer-accs/src/main/java/oracle/paas/accs/deployer/spi/app/ACCSTaskLauncher.java
+++ spring-cloud-deployer-accs/src/main/java/oracle/paas/accs/deployer/spi/app/ACCSTaskLauncher.java
@@ -1,22 +1,36 @@
 package oracle.paas.accs.deployer.spi.app;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import oracle.paas.accs.deployer.spi.accs.client.ACCSClient;
 import oracle.paas.accs.deployer.spi.accs.client.StorageClient;
 import oracle.paas.accs.deployer.spi.accs.model.Application;
+import oracle.paas.accs.deployer.spi.accs.model.ApplicationStatus;
 import oracle.paas.accs.deployer.spi.accs.util.ACCSUtil;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.springframework.cloud.deployer.spi.core.AppDeploymentRequest;
 import org.springframework.cloud.deployer.spi.core.RuntimeEnvironmentInfo;
 import org.springframework.cloud.deployer.spi.task.TaskLauncher;
 import org.springframework.cloud.deployer.spi.task.TaskStatus;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ACCSTaskLauncher implements TaskLauncher {
 
+    private static final int TIME_OUT_IN_MIN = 5;
     private RuntimeEnvironmentInfo runtimeEnvironmentInfo;
     private StorageClient storageClient;
     private ACCSClient accsClient;
@@ -33,59 +47,134 @@ public class ACCSTaskLauncher implements TaskLauncher {
         logger.log(Level.INFO, String.format("Entered deploy: Deploying AppDeploymentRequest: AppDefinition = {%s}, Resource = {%s}, Deployment Properties = {%s}",
                 appDeploymentRequest.getDefinition(), appDeploymentRequest.getResource(), appDeploymentRequest.getDeploymentProperties()));
 
-        String deploymentId = deploymentId(appDeploymentRequest);
-        logger.log(Level.INFO, String.format("deploy: Getting Status for Deployment Id = {%s}", deploymentId));
+        String appId = appId(appDeploymentRequest);
 
         try {
-            deployApplication(appDeploymentRequest, deploymentId);
+
+            logger.log(Level.INFO, String.format("Checking if app already exists = {%s}", appId));
+            if(!applicationExists(appId)) {
+                logger.log(Level.INFO, "Application doesn't exists. Creating new application");
+//                deployApplication(appDeploymentRequest, appId); -- This will be taken care later
+            }
+            if(!applicationInRunningState(appId)) {
+                waitForApplicationToFinish(appId);
+            }
+            launchTask(appDeploymentRequest, appId);
         } catch (Exception e) {
             e.printStackTrace();
             throw new IllegalStateException("Unable to launch task. " + e.getMessage());
         }
+
+        String deploymentId = deploymentId(appDeploymentRequest);
         logger.log(Level.INFO, String.format("Exiting deploy().  Deployment Id = {%s}", deploymentId));
         return deploymentId;
     }
 
-    private void deployApplication(AppDeploymentRequest appDeploymentRequest, String deploymentId) {
-        File file = null;
+
+    private void launchTask(AppDeploymentRequest appDeploymentRequest, String appId) {
         try {
-            file = appDeploymentRequest.getResource().getFile();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Exception while retrieving file", e);
-        }
-
-        if(file != null) {
+            String filename = appDeploymentRequest.getResource().getFilename();
+            String deploymentId = deploymentId(appDeploymentRequest);
             AppBuilder appBuilder = new AppBuilder(appDeploymentRequest, deploymentId);
-            String command = appBuilder.buildCommand(file.getName());
-
-            File zipFile = null;
-            try {
-                zipFile = ACCSUtil.convertToZipFile(file, command);
-                logger.log(Level.INFO, "Created zip file : " +zipFile.getAbsolutePath());
-                storageClient.pushFileToStorage(zipFile);
-                String appName = ACCSUtil.getSanitizedApplicationName(deploymentId);
-                if(!accsClient.applicationExists(appName)) {
-                    Application application = appBuilder.getApplicationData(zipFile.getName());
-                    accsClient.createApplication(application);
-                } else {
-                    Application application = appBuilder.getApplicationData(zipFile.getName());
-                    accsClient.updateApplication(application);
-                }
-            } catch (Exception se){
-                logger.log(Level.SEVERE, "Exception during file operation: " + zipFile.getAbsolutePath(), se);
+            String command = appBuilder.buildCommand(filename);
+            String mavenCoordinates = appDeploymentRequest.getResource().getURI().toString();
+            Client client = getClient();
+            Response response = null;
+            Task task = new Task(command, mavenCoordinates);
+            WebTarget webTarget = client.target(getUrl(appId) + "/tasks");
+            response = webTarget.request(MediaType.APPLICATION_JSON_TYPE)
+                    .post(Entity.entity(task, MediaType.APPLICATION_JSON_TYPE), Response.class);
+            logger.log(Level.INFO, response.getStatus() + " "
+                    + response.getStatusInfo() + " " + response);
+            if (!response.getStatusInfo().toString().equals(Response.Status.ACCEPTED.toString()) &&
+                    !response.getStatusInfo().toString().equals(Response.Status.OK.toString())) {
+                String outputString = response.readEntity(String.class);
+                throw new RuntimeException("Unable to launch task. " +  outputString);
             }
-            ACCSUtil.deleteFile(zipFile);
-            ACCSUtil.deleteCommonFiles();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Unable to launch task", e);
+            throw new RuntimeException(e);
         }
+    }
+
+    private static class Task {
+        private String mavenCoordinates;
+        private String command;
+
+        public Task(String command, String mavenCoordinates) {
+            this.command = command;
+            this.mavenCoordinates = mavenCoordinates;
+        }
+
+        public String getMavenCoordinates() {
+            return mavenCoordinates;
+        }
+
+        public void setMavenCoordinates(String mavenCoordinates) {
+            this.mavenCoordinates = mavenCoordinates;
+        }
+
+        public String getCommand() {
+            return command;
+        }
+
+        public void setCommand(String command) {
+            this.command = command;
+        }
+    }
+
+    private Client getClient() {
+        final ClientConfig config = new ClientConfig().register(JacksonJsonProvider.class);
+        return ClientBuilder.newClient(config);
+    }
+
+    private String appId(AppDeploymentRequest request) {
+        if(request.getDefinition() != null) {
+            Map<String, String> properties = request.getDefinition().getProperties();
+            return properties.get("spring.cloud.task.name") + "Launcher";
+        }
+        throw new RuntimeException("Request invalid");
     }
 
     private String deploymentId(AppDeploymentRequest request) {
         if(request.getDefinition() != null) {
             Map<String, String> properties = request.getDefinition().getProperties();
-            String taskName = properties.get("spring.cloud.task.name");
-            return taskName;
+            return properties.get("spring.cloud.task.name") + properties.get("spring.cloud.task.executionid");
         }
         throw new RuntimeException("Request invalid");
+    }
+
+    private boolean applicationExists(String appId) {
+        return accsClient.applicationExists(appId);
+    }
+
+    private boolean applicationInRunningState(String appId) {
+        ApplicationStatus applicationStatus =  accsClient.getApplication(appId);
+        if(applicationStatus != null) {
+            return applicationStatus.getStatus().equalsIgnoreCase("RUNNING");
+        }
+        return false;
+    }
+
+    private String getUrl(String appId) {
+        ApplicationStatus applicationStatus =  accsClient.getApplication(appId);
+        if(applicationStatus != null) {
+            return applicationStatus.getWebURL();
+        }
+        return null;
+    }
+
+    private void waitForApplicationToFinish(String appId) throws  Exception {
+        Date currentTime = Calendar.getInstance().getTime();
+        long maxTimeToWait = currentTime.getTime() + TIME_OUT_IN_MIN * 60 * 1000;
+        boolean appRunning = false;
+        while(!appRunning && maxTimeToWait > Calendar.getInstance().getTime().getTime()) {
+            Thread.sleep(15000);
+            appRunning = applicationInRunningState(appId);
+        }
+        if(!appRunning) {
+            throw new Exception("Unable to get launcher app running. Can't Launch task.");
+        }
     }
 
     public void cancel(String s) {
